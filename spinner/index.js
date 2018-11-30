abd6573f9bb4501d3f2356e855706504d01ab80a
Alessio Carnevale <alessio.carnevale@gmail.com>
2018-11-30 21:10:55 +0100

    refactoring: adding `attachTo` method to create a new instance

--- spinner/index.js
+++ spinner/index.js
@@ -3,9 +3,40 @@ if (window) {
 }
 
 module.exports = {
-  show: (msg = 'Loading file') => {
-    document.documentElement.style.setProperty('--loading-message', `"${msg}"`)
-    document.body.classList.remove('content-ready')
-  },
-  hide: () => { document.body.classList.add('content-ready') }
+  attachTo: (container = document.body) => {
+    const spinner = document.createElement('div')
+    let attached = false
+
+    const updateSpinner = msg => {
+      const { top, left, width, height } = container.getBoundingClientRect()
+
+      spinner.style.cssText = `
+        top:${top}px;
+        left:${left}px;
+        height:${height}px;
+        width:${width}px;
+        --loading-message: "${msg}";
+      `
+    }
+    return {
+      show: (msg = 'Loading file...') => {
+        if (attached) {
+          updateSpinner(msg)
+          return
+        }
+        container.classList.add('_loading_spinner_container')
+        spinner.classList.add('_loading_spinner_')
+
+        updateSpinner(msg)
+        document.body.appendChild(spinner)
+
+        attached = true
+      },
+      hide: () => {
+        document.body.removeChild(spinner)
+        container.classList.remove('_loading_spinner_container')
+        attached = false
+      }
+    }
+  }
 }
