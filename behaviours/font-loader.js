c5e493e638814049a29e4d6831a17abfa2e81d0a
Stuart Wilson <stuart.wilson@nearform.com>
2019-01-23 12:33:12 +0000

    [647] - Allow passthrough of opts to webfontloader

--- behaviours/font-loader.js
+++ behaviours/font-loader.js
@@ -2,14 +2,14 @@
 
 const WebFont = require('webfontloader')
 
-const init = ({ onLoad } = {}) => {
-  const timeoutId = setTimeout(() => active(), 2000)
+const init = ({ onLoad, ...opts } = {}) => {
+  const timeoutId = setTimeout(() => active('Timeout'), 2000)
 
-  const active = () => {
+  const active = err => {
     clearTimeout(timeoutId)
 
     if (onLoad) {
-      onLoad()
+      onLoad(err)
     }
   }
 
@@ -20,7 +20,8 @@ const init = ({ onLoad } = {}) => {
       families: ['Archia:n4,n7', 'Campton:n7', 'Space Mono:n4,n7'],
       urls: ['https://adoring-einstein-015923.netlify.com/assets/css/fonts.css']
     },
-    active
+    active,
+    ...opts
   })
 
   return {
