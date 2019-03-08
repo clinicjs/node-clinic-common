a0655cf1a4aa7c6453e94104f2af33b84e2c6d7e
Alessio Carnevale <alessio.carnevale@gmail.com>
2019-03-08 16:10:13 +0100

    fixing backdrop opacity

--- base/element-highlighter.js
+++ base/element-highlighter.js
@@ -4,12 +4,15 @@ const wrapper = document.createElement('div')
 wrapper.classList.add('nc-element-highlighter')
 
 wrapper.innerHTML = `
-  <div class="nc-element-highlighter-panel nc-element-highlighter-top"></div>
-  <div class="nc-element-highlighter-panel nc-element-highlighter-right"></div>
-  <div class="nc-element-highlighter-panel nc-element-highlighter-bottom"></div>
-  <div class="nc-element-highlighter-panel nc-element-highlighter-left"></div>
+  <div class="nc-element-highlighter-container">
+    <div class="nc-element-highlighter-panel nc-element-highlighter-top"></div>
+    <div class="nc-element-highlighter-panel nc-element-highlighter-right"></div>
+    <div class="nc-element-highlighter-panel nc-element-highlighter-bottom"></div>
+    <div class="nc-element-highlighter-panel nc-element-highlighter-left"></div>
+  </div>
   <div class="nc-element-highlighter-border"></div>
 `
+const backdropContainer = wrapper.querySelector('.nc-element-highlighter-container')
 const backdropTop = wrapper.querySelector('.nc-element-highlighter-top')
 const backdropRight = wrapper.querySelector('.nc-element-highlighter-right')
 const backdropBottom = wrapper.querySelector('.nc-element-highlighter-bottom')
@@ -29,7 +32,7 @@ const elementHighLighter = {
   },
 
   hide: () => {
-    wrapper.style.cssText = `opacity:0`
+    backdropContainer.style.cssText = `opacity:0.6;`
     wrapper.classList.remove('show')
   },
 
@@ -39,11 +42,11 @@ const elementHighLighter = {
 
     wrapper.classList.toggle('showBorder', showBorder)
     const pos = element.getBoundingClientRect()
-
-    backdropTop.style.cssText = `opacity:0.6; transform:translate3d(${pos.left - padding}px, calc(-100vh + ${pos.top - padding}px), 0)`
-    backdropRight.style.cssText = `opacity:0.6; transform:translate3d(${pos.left + pos.width + padding}px, ${pos.top - padding}px, 0)`
-    backdropBottom.style.cssText = `opacity:0.6; transform:translate3d(calc(-100vw + ${pos.left + pos.width + padding}px), ${pos.top + pos.height + padding}px, 0)`
-    backdropLeft.style.cssText = `opacity:0.6; transform:translate3d(calc(-100vw + ${pos.left - padding}px), calc(-100vh + ${pos.top + pos.height + padding}px), 0)`
+    backdropContainer.style.cssText = `opacity:0.6;`
+    backdropTop.style.cssText = `transform:translate3d(${pos.left - padding}px, calc(-100vh + ${pos.top - padding}px), 0)`
+    backdropRight.style.cssText = `transform:translate3d(${pos.left + pos.width + padding}px, ${pos.top - padding}px, 0)`
+    backdropBottom.style.cssText = `transform:translate3d(calc(-100vw + ${pos.left + pos.width + padding}px), ${pos.top + pos.height + padding}px, 0)`
+    backdropLeft.style.cssText = `transform:translate3d(calc(-100vw + ${pos.left - padding}px), calc(-100vh + ${pos.top + pos.height + padding}px), 0)`
 
     if (showBorder) {
       backdropBorder.style.cssText = `left:${pos.left - padding}px;top:${pos.top - padding}px;width:${pos.width + padding * 2}px;height:${pos.height + padding * 2}px;`
